13fa569e4e274d54fc4031c84d579d93aa1b8847
dingduck <kfove.liang@gmail.com>
2023-08-12 18:58:05 +0800

    panic handler

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -1,7 +1,19 @@
+use crate::println;
+use crate::sbi::shut_down;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    // 先不做任何处理
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "[kernel] Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("[kernel] Panicked: {}", info.message().unwrap());
+    }
+
+    shut_down()
 }
